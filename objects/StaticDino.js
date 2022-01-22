54b0aedde82bccc8e78ceb165eb6d51d54095032
অর্ণব <78209407+Arnob17@users.noreply.github.com>
2022-01-22 18:44:29 +0600

    xd
    xd

---
+++ objects/StaticDino.js
@@ -0,0 +1,106 @@
+exports.dino_static = [
+    {
+        name: "Venzy",
+        id: "venzy01",
+        rarity: "🟣 Epic",
+        aboutme: "Venzy is a lazy and sleepy bug but\n when he gets angry he can blast anyone to death.",
+        type: "Bug",
+        picture: "https://cdn.discordapp.com/attachments/616652509385064484/860773905667981312/SPOILER_unknown.png",
+    },
+    {
+        name: "Margarin",
+        id: "margarin02",
+        rarity: "🔴 Mythic",
+        aboutme: "Margerine is a dino who is Smooth like butter.\n He throws melting butter at enimies.",
+        type: "No info",
+        picture: "https://cdn.discordapp.com/attachments/730690876367700009/859457602307227668/unknown.png"
+    },
+    {
+        name: "Groduk",
+        id: "gorduk01",
+        rarity: "🟣 Epic",
+        aboutme: "Groduk is the scariest dino, \nhis body glows in the dark which frightens everyone.",
+        type: "Ghost",
+        picture: "https://cdn.discordapp.com/attachments/850720537252069416/859695999223070750/image_16.png"
+    },
+    {
+        name: "Angy",
+        id: "angy04",
+        rarity: "🟢 Common",
+        aboutme: "Angy gets angry very easily.. \nhis Angryness causes him to shoot FIRE out of his mouth.",
+        type: "Fire",
+        picture: "https://cdn.discordapp.com/attachments/906068154961260585/906068166122291220/unknown.png"
+    },
+    {
+        name: "Steldro",
+        id: "steldro05",
+        rarity: "🟠 Rare",
+        aboutme: "Steldro is made out of metal\n and has STRONG horns in his head as well as a STRONG tail.",
+        type: "Steel",
+        picture: "https://cdn.discordapp.com/attachments/859696736334250004/911307764012285992/unknown.png"
+    },
+    {
+        name: "ZAP",
+        id: "zap06",
+        rarity: "🟠 Rare",
+        aboutme: "Zap is Fast as light and ready to fight!\n The secret behind is speed has always been a mystery",
+        type: "Electric",
+        picture: "https://cdn.discordapp.com/attachments/859696736334250004/859697942747283476/unknown.png"
+    },
+    {
+        name: "Dark Voint",
+        id: "darkvoint07",
+        rarity: "🔴 Mythic",
+        aboutme: "DarkVoint is the darkest dino, \nHe always likes to stay in dark places, \nHe fight with his dark energy.",
+        type: "Dark",
+        picture: "https://cdn.discordapp.com/attachments/859696736334250004/911895198789935104/unknown.png"
+    },
+    {
+        name: "Jefly",
+        id: "jefly08",
+        rarity: "🟢 Common",
+        aboutme: "Jefly can fly with his amazing wings and has sharp fangs which he can use in strange moments.",
+        type: "Flying",
+        picture: "https://cdn.discordapp.com/attachments/906068154961260585/906075172069601300/unknown.png"
+    },
+    {
+        name: "Ski",
+        id: "ski10",
+        rarity: "🟢 Common",
+        aboutme: "Ski lives in beach and loves\n to swim in water..\nHe shoots water as his attack move.",
+        type: "Water",
+        picture: "https://media.discordapp.net/attachments/859696736334250004/860761328271491082/unknown.png?width=183&height=98"
+    },
+    {
+        name: "Frizy",
+        id: "frizy11",
+        rarity: "🟣 Epic",
+        aboutme: "Frizy is a ice type dino.\n Even tho he is an ice type,\n He gets sick because of the cold weather. \nHe likes to stay near fireplaces to warm his body.",
+        type: "Ice",
+        picture: "https://media.discordapp.net/attachments/906068154961260585/908329668984340500/SPOILER_unknown.png?width=272&height=459"
+    },
+    {
+        name: "Ellenora",
+        id: "ellenora12",
+        rarity: "🟣 Epic",
+        aboutme: "Ellenora is a fairy dino.\n Even she is the queen in fairy land, \n Jefly always try to protect her.\n If you even touch her, Jefly will kill you!",
+        type: "Fairy",
+        picture: "https://cdn.discordapp.com/attachments/859696736334250004/911310169068163153/unknown.png"
+    },
+    {
+        name: "Mirga",
+        id: "mirga13",
+        rarity: "🟡 Legendary",
+        aboutme: "Mirga is a cook. When she gets angry,\n she attacks ROASTS the opponent with her Frying pan.",
+        type: "Fire",
+        picture: "https://media.discordapp.net/attachments/730690876367700009/906158905430798376/dino.png?width=553&height=580"
+    },
+    {
+        name: "Frisky",
+        id: "frisky14",
+        rarity: "🟠 Rare",
+        aboutme: "Frisky is cool and swaggy, in fact he is the chillest dino who has no fear of danger. He enjoys his life in the beach by surfing in the water and drinking juice",
+        type: "Normal",
+        picture: "https://cdn.discordapp.com/attachments/906068154961260585/908333937753989220/Dino.png"
+    }
+]
---
+++ objects/storeitem.js
@@ -0,0 +1,79 @@
+exports.store_item = [
+    {
+        name: "Small Mystery Box",
+        price: 170,
+        icon: "<:small_box:909738195783139358>",
+        id: "smallmysterybox",
+        type: "item"
+    },
+    {
+        name: "Big Mystery Box",
+        price: 370,
+        icon: "<:big_box:909738337059872799>",
+        id: "bigmysterybox",
+        type: "item"
+    },
+    {
+        name: "Dino food (veg)",
+        price: 20,
+        icon: "<:vegDinoFood:909833466126934066>",
+        id: "vegdinofood",
+        type: "item"
+    },
+    {
+        name: "Dino food (non-veg)",
+        price: 50,
+        icon: "<:npnvegDinofood:909833764023201872>",
+        id: "nonvegdinofood",
+        type: "item"
+    },
+    {
+        name: "Frisky",
+        price: 4000,
+        icon: "<:Frisky:906573844872060968>",
+        id: "dinofrisky",
+        type: "dino"
+    },
+    {
+        name: "Jefly",
+        price: 2000,
+        icon: "<:Jefly:869805313027616788>",
+        id: "dinojefly",
+        type: "dino"
+    },
+    {
+        name: "Ski",
+        price: 2000,
+        icon: "<:Ski:869805410645844009>",
+        id: "dinoski",
+        type: "dino"
+    },
+    {
+        name: "ZAP",
+        price: 5000,
+        icon: "<:Zap:869805587343487088>",
+        id: "dinozap",
+        type: "dino"
+    },
+    {
+        name: "Mirga",
+        price: 8000,
+        icon: "<:Mirga:906424805954314252>",
+        id: "dinomirga",
+        type: "dino"
+    },
+    {
+        name: "Angy",
+        price: 2000,
+        icon: "<:Angy:869805228344619029>",
+        id: "dinoangy",
+        type: "dino"
+    },
+    {
+        name: "Margarin",
+        price: 5000,
+        icon: "<:Margarin:869805363640283146>",
+        id: "dinomargerin",
+        type: "dino"
+    },
+];
