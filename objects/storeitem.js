b8c32abeb6f55cc2b81a327b92a4f535e588446e
snixon <94747670+snixu@users.noreply.github.com>
2022-01-22 20:41:13 +0600

    Update storeitem.js

--- objects/storeitem.js
+++ objects/storeitem.js
@@ -29,14 +29,14 @@ exports.store_item = [
     },
     {
         name: "Frisky",
-        price: 4000,
+        price: 5000,
         icon: "<:Frisky:906573844872060968>",
         id: "dinofrisky",
         type: "dino"
     },
     {
         name: "Jefly",
-        price: 2000,
+        price: 5000,
         icon: "<:Jefly:869805313027616788>",
         id: "dinojefly",
         type: "dino"
@@ -50,14 +50,14 @@ exports.store_item = [
     },
     {
         name: "ZAP",
-        price: 5000,
+        price: 15000,
         icon: "<:Zap:869805587343487088>",
         id: "dinozap",
         type: "dino"
     },
     {
         name: "Mirga",
-        price: 8000,
+        price: 30000,
         icon: "<:Mirga:906424805954314252>",
         id: "dinomirga",
         type: "dino"
@@ -71,9 +71,52 @@ exports.store_item = [
     },
     {
         name: "Margarin",
-        price: 5000,
+        price: 20000,
         icon: "<:Margarin:869805363640283146>",
         id: "dinomargerin",
         type: "dino"
     },
-];
+    {
+        name: "Steldro",
+        price: 10000,
+        icon: "<:Steldro:869805493282033674>",
+        id: "dinosteldro05",
+        type: "dino"
+    },
+  {
+        name: "Venzy",
+        price: 10000,
+        icon: "<venzy:869805562060218388>",
+        id: "dinovenzy01",
+        type: "dino"
+    },
+  {
+        name: "Groduk",
+        price: 10000,
+        icon: "<:Groduk:869805274670727179>",
+        id: "dinogorduk01",
+        type: "dino"
+    },
+  {
+        name: "Dark Voint",
+        price: 20000,
+        icon: "<:DarkVoint:869805167019704351>",
+        id: "darkvoint07",
+        type: "dino"
+    },
+  {
+        name: "Frizy",
+        price: 10000,
+        icon: "<:frizy:906424805706833961>",
+        id: "dinofrizy11",
+        type: "dino"
+    },
+  {
+        name: "Ellenora",
+        price: 10000,
+        icon: "<:Ellenora:906425773555060787>",
+        id: "ellenora12",
+        type: "dino"
+    },
+},
+  
